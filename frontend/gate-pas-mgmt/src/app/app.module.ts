95258ec0331d35e859cba95d00eac334e93cade2
saurabh jog <saurabhjog2002@gmail.com>
2023-07-14 12:39:23 +0530

    added angular material ui

--- frontend/gate-pas-mgmt/src/app/app.module.ts
+++ frontend/gate-pas-mgmt/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './component/user-login/user-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { UserLoginComponent } from './component/user-login/user-login.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
