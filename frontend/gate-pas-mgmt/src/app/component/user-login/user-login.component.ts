f9983f904bcb914f72be5a15486a46b7fed97c43
saurabh jog <saurabhjog2002@gmail.com>
2023-07-14 15:36:06 +0530

    Changed alert style

--- frontend/gate-pas-mgmt/src/app/component/user-login/user-login.component.ts
+++ frontend/gate-pas-mgmt/src/app/component/user-login/user-login.component.ts
@@ -7,7 +7,8 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./user-login.component.scss']
 })
 export class UserLoginComponent {
-  alerttext = '';
+  alerttext = "";
+  alertBgColor = "";
   id = new FormControl("");
   password = new FormControl("");
 
@@ -16,8 +17,10 @@ export class UserLoginComponent {
     const password = this.password.value;
     if (id === "" || password === "") {
       this.alerttext = (id === ""? "Student Id": "Password") + " cannot be empty";
+      this.alertBgColor = "white";
       setTimeout(() => {
         this.alerttext = "";
+        this.alertBgColor = "";
       }, 1000)
       return;
     }
